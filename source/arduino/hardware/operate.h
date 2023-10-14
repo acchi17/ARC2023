529fa4e7448011cfb90977db9fdfc48bb648d368
pisce <pisce@ymail.ne.jp>
2023-10-14 21:08:49 +0900

    add constant value and enum

---
+++ source/arduino/hardware/operate.h
@@ -0,0 +1,43 @@
+// define pin
+#define HEATER       0
+#define ROBOT_ARM    0
+#define ROBOT_HAND   0
+#define L_CATEPILLAR 0
+#define R_CATEPILLAR 0
+
+// define PWM
+
+// default
+#define PWM_ARM_DEF    255
+#define PWM_HAND_DEF   255
+#define PWM_HEATER_DEF 255
+#define PWM_L_CATE_DEF 255
+#define PWM_R_CATE_DEF 255
+
+// define enum
+
+// operate catepillar
+
+// plan 1
+typedef enum {
+    REST,
+    FORWARD,
+    BACKWARD,
+    RIGHT,
+    LEFT,
+} direction;
+
+// plan 2
+
+// type enum {
+//     REST,       // Rest
+//     LFWD,       // Left  Forward
+//     RFWD,       // Right Forward
+//     LBWD,       // Left  Backward
+//     RBWD,       // Right Backward
+//     LTRN,       // Left  Turn
+//     RTRN,       // Right Turn
+// } derection;
+    
+
+
