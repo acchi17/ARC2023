32233e11aed39757443004f0782172e5124e0e8e
pisce <pisce@ymail.ne.jp>
2023-10-15 11:35:34 +0900

    modify pinMode and add operation

--- source/arduino/hardware/operate.ino
+++ source/arduino/hardware/operate.ino
@@ -3,11 +3,15 @@
 // setup
 void init() {
     // pin mode
-    pinMode(HEATER,       OUTPUT);
-    pinMode(ROBOT_ARM,    OUTPUT);
-    pinMode(ROBOT_HAND,   OUTPUT);
-    pinMode(L_CATEPILLAR, OUTPUT);
-    pinMode(R_CATEPILLAR, OUTPUT);
+    pinMode(HEATER,           OUTPUT);
+    pinMode(ROBOT_ARM,        OUTPUT);
+    pinMode(ROBOT_ARM_REV,    OUTPUT);
+    pinMode(ROBOT_HAND,       OUTPUT);
+    pinMode(ROBOT_HAND_REV,   OUTPUT);
+    pinMode(L_CATEPILLAR,     OUTPUT);
+    pinMode(L_CATEPILLAR_REV, OUTPUT);
+    pinMode(R_CATEPILLAR,     OUTPUT);
+    pinMode(R_CATEPILLAR_REV, OUTPUT);
 }
 
 // operate
@@ -19,6 +23,16 @@ bool contrl_catepillar(direction dir) {
 
 bool contrl_robot_arm(void) {
     // control robot arm!
+    return true;
+}
+
+bool contrl_robot_hand(void) {
+    // control robot hand
+    return true;
+}
+
+bool contrl_heater(bool is_on) {
+    // control heater
     return true;
 }
 
