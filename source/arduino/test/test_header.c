f42f2b37db4e7cfe0d1007e8d0234ab79e4ec103
picse <pisce@ymail.ne.jp>
2023-10-21 23:27:10 +0900

    add tester

--- source/arduino/test/test_header.c
+++ source/arduino/test/test_header.c
@@ -2,6 +2,37 @@
 #include <stdbool.h>
 #include "../hardware/operate.h"
 
+typedef struct {
+    short ln;
+    short lr;
+    short rn;
+    short rr;
+} signal;
+
+bool get_cmd(opr op, signal* sig) {
+    char ln, lr, rn, rr;
+
+    ln = (op & OPR_L_NORMAL)  == OPR_L_NORMAL;
+    lr = (op & OPR_L_REVERSE) == OPR_L_REVERSE;
+    rn = (op & OPR_R_NORMAL)  == OPR_R_NORMAL;
+    rr = (op & OPR_R_REVERSE) == OPR_R_REVERSE;
+
+    sig->ln = (int)ln;
+    sig->lr = (int)lr;
+    sig->rn = (int)rn;
+    sig->rr = (int)rr;
+    return;
+}
+
+void print_signal(char op) {
+    signal sig;
+    get_cmd(op, &sig);
+
+    printf(" = %d %d %d %d\n", sig.ln, sig.lr, sig.rn, sig.rr);
+    return;
+};
+
+
 int main() {
     printf("PWM_DEFAULT     = %x\n", PWM_DEFAULT);
     printf("PWM_ARM_DEFAULT = %x\n", PWM_DEFAULT);
@@ -10,15 +41,22 @@ int main() {
     printf("HAND_NORMAL  = %x\n", HAND_OPEN);
     printf("HAND_REVERSE = %x\n", HAND_CLOSE);
 
-    printf("CTPL_REST    = %x\n", CTPL_REST);
-    printf("CTPL_FWRD    = %x\n", CTPL_FWRD);
-    printf("CTPL_LFWD    = %x\n", CTPL_LFWD);
-    printf("CTPL_RFWD    = %x\n", CTPL_RFWD);
-    printf("CTPL_LBWD    = %x\n", CTPL_LBWD);
-    printf("CTPL_RBWD    = %x\n", CTPL_RBWD);
-    printf("CTPL_LTRN    = %x\n", CTPL_LTRN);
-    printf("CTPL_RTRN    = %x\n", CTPL_RTRN);
-
+    printf("CTPL_REST    = %x", CTPL_REST);
+    print_signal(CTPL_REST);
+    printf("CTPL_FWRD    = %x", CTPL_FWRD);
+    print_signal(CTPL_FWRD);
+    printf("CTPL_LFWD    = %x", CTPL_LFWD);
+    print_signal(CTPL_LFWD);
+    printf("CTPL_RFWD    = %x", CTPL_RFWD);
+    print_signal(CTPL_RFWD);
+    printf("CTPL_LBWD    = %x", CTPL_LBWD);
+    print_signal(CTPL_LBWD);
+    printf("CTPL_RBWD    = %x", CTPL_RBWD);
+    print_signal(CTPL_RBWD);
+    printf("CTPL_LTRN    = %x", CTPL_LTRN);
+    print_signal(CTPL_LTRN);
+    printf("CTPL_RTRN    = %x", CTPL_RTRN);
+    print_signal(CTPL_RTRN);
     
     return 0;
 }
