f1f742cef168d527b57025dc1378723a12a58e84
picse <pisce@ymail.ne.jp>
2023-11-05 17:38:25 +0900

    change cp to ln

--- SetupAutoStart.sh
+++ SetupAutoStart.sh
@@ -2,5 +2,6 @@
 
 systemctl stop arc2023
 cp arc2023.service /etc/systemd/system/
+ln -sf /home/arc/ARC2023/arc2023.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl start arc2023.service
